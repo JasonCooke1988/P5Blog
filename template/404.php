ab5662f55a5c72d1178e854516d5b194f2d1f88f
Jason <jason.cooke@hotmail.fr>
2020-12-04 14:48:48 +0100

    Create custom 404 page

--- template/404.php
+++ template/404.php
@@ -1,1 +1,10 @@
-<div>404 Not found</div>
+<?php
+require_once('head.php');
+
+use App\Helper\Template; ?>
+
+<div class="404-error">
+    <p class="error-text d-flex justify-content-center">Cette page n'existe pas. Cliquez <a class="px-2" href="<?= Template::getBasePath() ?>">ICI</a> pour retourner a l'accueil</p>
+</div>
+
+<?php require_once('footer.php'); ?>
