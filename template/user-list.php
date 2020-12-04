efccae14f849b214f52747827262d2d012da5677
Jason <jason.cooke@hotmail.fr>
2020-12-04 14:34:17 +0100

    Set user as admin Create function to set users as admin by an admin

---
+++ template/user-list.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Helper\Template;
+use App\Model\Post;
+
+/**
+ * @var Post[] $posts
+ */
+require_once('head.php');
+?>
+
+<!-- Main Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <?php if (isset($error)): ?>
+                <div class="form-error">
+                    <p id="form-error" class="error-message"><?= $error ?></p>
+                </div>
+            <?php endif ?>
+            <?php if (isset($success)): ?>
+                <div class="form-success">
+                    <p id="form-success" class="success-message"><?= $success ?></p>
+                </div>
+            <?php endif ?>
+            <p>Cliquez sur "Désigner comme administrateur" pour définir cet utilisateur en tant que administrateur</p>
+            <?php
+            foreach ($users as $user):
+                ?>
+            <?php if(!$user->isRoleAdmin()): ?>
+                <div class="user">
+                        <p><?= $user->getFullName() ?></p>
+                    <form action="<?= Template::getBasePath() . '/user-list/' . $user->getId() ?>" id="set-admin" method="post">
+                        <input type="submit" value="Désigner comme administrateur">
+                    </form>
+                </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+
+<hr>
+<?php require_once('footer.php'); ?>
